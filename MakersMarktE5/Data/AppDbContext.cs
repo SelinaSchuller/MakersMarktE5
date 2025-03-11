bc2129124a0494a5c8d0ac84b7d4672f13c22b16
SelinaSchuller <115994002+SelinaSchuller@users.noreply.github.com>
2025-03-11 09:37:42 +0100

    Namen aangepast

--- MakersMarktE5/Data/AppDbContext.cs
+++ MakersMarktE5/Data/AppDbContext.cs
@@ -14,7 +14,7 @@ namespace MakersMarktE5.Data
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseMySql(
-				ConfigurationManager.ConnectionStrings["BarrocIntens"].ConnectionString,
+				ConfigurationManager.ConnectionStrings["MakersMarkt"].ConnectionString,
 				ServerVersion.Parse("8.0.30")
 				);
 		}
