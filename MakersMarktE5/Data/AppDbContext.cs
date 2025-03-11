1427a820dcff8d7483d08aafacaad8848d1a95e5
SelinaSchuller <115994002+SelinaSchuller@users.noreply.github.com>
2025-03-11 09:34:02 +0100

    AppDbContext aangemaakt en App.Config
    Je moet voor jezelf de ExampleApp.Config, het stukje ' Example' weghalen en dan jou inlog gegevens invullen.

--- MakersMarktE5/Data/AppDbContext.cs
+++ MakersMarktE5/Data/AppDbContext.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MakersMarktE5.Data
 {
-    class AppDbContext
-    {
-    }
+	class AppDbContext : DbContext
+	{
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql(
+				ConfigurationManager.ConnectionStrings["BarrocIntens"].ConnectionString,
+				ServerVersion.Parse("8.0.30")
+				);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+	}
 }
