a49cb5c6b0d18b1822914eb64074dda6e8c48942
SelinaSchuller <115994002+SelinaSchuller@users.noreply.github.com>
2025-03-18 10:43:18 +0100

    Pagine gedeelte toegevoegd

---
+++ MakersMarktE5/Services/BooleanToYesNoConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakersMarktE5.Services
+{
+	public class BooleanToYesNoConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return (bool)value ? "Yes" : "No";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return (string)value == "Yes";
+		}
+	}
+}
--- MakersMarktE5/Views/ModeratorViews/VerifyAccountProductPage.xaml.cs
+++ MakersMarktE5/Views/ModeratorViews/VerifyAccountProductPage.xaml.cs
@@ -1,31 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.EntityFrameworkCore;
+using MakersMarktE5.Data;
 
 namespace MakersMarktE5.Views.ModeratorViews
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class VerifyAccountProductPage : Page
-    {
-        public VerifyAccountProductPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	public sealed partial class VerifyAccountProductPage : Page
+	{
+		public ObservableCollection<User> UnverifiedUsers { get; set; } = new ObservableCollection<User>();
+		public ObservableCollection<Product> UnverifiedProducts { get; set; } = new ObservableCollection<Product>();
+
+		public VerifyAccountProductPage()
+		{
+			this.InitializeComponent();
+			LoadData();
+		}
+
+		private void LoadData()
+		{
+			using(var db = new AppDbContext())
+			{
+				// Fetch Unverified Users
+				var userList = db.Users.Include(u => u.Role).Where(u => !u.IsVerified).ToList();
+				UnverifiedUsers.Clear();
+				foreach(var user in userList)
+				{
+					UnverifiedUsers.Add(user);
+				}
+
+				// Fetch Unverified Products with Includes
+				var productList = db.Products
+									.Include(p => p.Type)
+									.Include(p => p.Materials)
+									.Include(p => p.Categories)
+									.Include(p => p.UniqueProperty)
+									.Include(p => p.Creator)
+									.Where(p => !p.IsVerified)
+									.ToList();
+				UnverifiedProducts.Clear();
+				foreach(var product in productList)
+				{
+					UnverifiedProducts.Add(product);
+				}
+			}
+
+			// Bind Data to UI
+			UnverifiedUserListView.ItemsSource = UnverifiedUsers;
+			ProductListView.ItemsSource = UnverifiedProducts;
+		}
+	}
 }
